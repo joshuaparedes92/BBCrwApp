593286a15eb43b0dc8844070dd886d0fd827a80f
Joshua Paredes <joshuaparedes92@gmail.com>
2016-03-09 18:39:47 -0500

    Changed the Drag items on page two completely. -Created Containers to hold the TextFields -Need to Fix Margins on Lines
    Implemented the Network Chart
    -Chart holds various elements to help the user pick out a networking solution for the customer.
    -Char elements are highlighted green when the user selects them to make the customer aware of which networking solution they should choose.

--- app/src/main/java/com/example/joshua/bbcrwapp/Main2Activity.java
+++ app/src/main/java/com/example/joshua/bbcrwapp/Main2Activity.java
@@ -30,10 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener
-, View.OnTouchListener {
-
-    TextView text1;
-
+ {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +39,44 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        findViewById(R.id.textView25).setOnClickListener(ColorChange);
+        findViewById(R.id.Size1).setOnClickListener(ColorChange);
+        findViewById(R.id.Size2).setOnClickListener(ColorChange);
+        findViewById(R.id.Size3).setOnClickListener(ColorChange);
+        findViewById(R.id.SecondCol1).setOnClickListener(ColorChange);
+        findViewById(R.id.SecondCol2).setOnClickListener(ColorChange);
+        findViewById(R.id.SecondCol3).setOnClickListener(ColorChange);
+        findViewById(R.id.ThirdCol1).setOnClickListener(ColorChange);
+        findViewById(R.id.ThirdCol2).setOnClickListener(ColorChange);
+        findViewById(R.id.ThirdCol3).setOnClickListener(ColorChange);
+        findViewById(R.id.FourthCol1).setOnClickListener(ColorChange);
+        findViewById(R.id.FourthCol2).setOnClickListener(ColorChange);
+        findViewById(R.id.FourthCol3).setOnClickListener(ColorChange);
+
+
+        findViewById(R.id.textView3).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView5).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView6).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView7).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView8).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView9).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView10).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView12).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView13).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView14).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView15).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView16).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView17).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView18).setOnTouchListener(new MyTouchListener());
+        findViewById(R.id.textView19).setOnTouchListener(new MyTouchListener());
+
+
+        findViewById(R.id.topLeft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.topRight).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomLeft).setOnDragListener(new MyDragListener());
+        findViewById(R.id.bottomRight).setOnDragListener(new MyDragListener());
 
         //Draggable Initialize
-        findViewById(R.id.textView3).setOnTouchListener(this);
+        /*findViewById(R.id.textView3).setOnTouchListener(this);
         findViewById(R.id.textView5).setOnTouchListener(this);
         findViewById(R.id.textView6).setOnTouchListener(this);
         findViewById(R.id.textView7).setOnTouchListener(this);
@@ -59,7 +90,7 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
         findViewById(R.id.textView16).setOnTouchListener(this);
         findViewById(R.id.textView17).setOnTouchListener(this);
         findViewById(R.id.textView18).setOnTouchListener(this);
-        findViewById(R.id.textView19).setOnTouchListener(this);
+        findViewById(R.id.textView19).setOnTouchListener(this);*/
 
 
         //YTM_Spinner1 Initialization
@@ -178,7 +209,6 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -191,23 +221,20 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
-    float x,y= 0.0f;
-    boolean moving = false;
+
+    /*boolean moving = false;
+    float x,y = 0.0f;
     @Override
     public boolean onTouch(View v, MotionEvent e){
         switch(e.getAction()){
             case MotionEvent.ACTION_DOWN:
-                moving = true;
-                x= e.getRawX()-60;
-                y= e.getRawY()-550;
-                v.setX(x);
-                v.setY(y);
+                moving= true;
                 v.getParent().requestDisallowInterceptTouchEvent(true);
                 break;
             case MotionEvent.ACTION_MOVE:
                 if(moving){
-                    x= e.getRawX()-60;
-                    y= e.getRawY()-550;
+                    x= e.getRawX() - v.getWidth();
+                    y= e.getRawY() - v.getHeight()-700;
                     v.setX(x);
                     v.setY(y);
                 }
@@ -217,20 +244,169 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
                 break;
         }
         return true;
-    }
+    }*/
 
-    private boolean stateChanged;
+
+     public final class MyTouchListener implements View.OnTouchListener{
+         public boolean onTouch(View view, MotionEvent motionEvent){
+             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                 ClipData data = ClipData.newPlainText("", "");
+                 View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                 view.startDrag(data, shadowBuilder, view, 0);
+                 view.setVisibility(View.INVISIBLE);
+                 return true;
+             }else{
+                 return false;
+             }
+         }
+     }
+
+     class MyDragListener implements View.OnDragListener {
+         @Override
+         public boolean onDrag(View v, DragEvent event) {
+             int action = event.getAction();
+             switch (event.getAction()){
+                 case DragEvent.ACTION_DRAG_STARTED:
+                     //do nothing
+                     break;
+                 case DragEvent.ACTION_DRAG_ENTERED:
+                     //do nothing
+                     break;
+                 case DragEvent.ACTION_DRAG_EXITED:
+                     //do nothing
+                     break;
+                 case DragEvent.ACTION_DROP:
+                     View view = (View) event.getLocalState();
+                     ViewGroup owner = (ViewGroup) view.getParent();
+                     owner.removeView(view);
+                     LinearLayout container = (LinearLayout) v;
+                     container.addView(view);
+                     view.setVisibility(View.VISIBLE);
+                     break;
+                 case DragEvent.ACTION_DRAG_ENDED:
+                     //do nothing
+                     default:
+                         break;
+             }
+             return true;
+         }
+     }
+
+
+    private boolean stateChanged1, stateChanged2, stateChanged3, stateChanged4, stateChanged5;
+    private boolean stateChanged6, stateChanged7, stateChanged8, stateChanged9, stateChanged10;
+    private boolean stateChanged11, stateChanged12;
+
     private View.OnClickListener ColorChange = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch(v.getId()){
-                case R.id.textView25:
-                    if(stateChanged){
-                        v.setBackgroundColor(Color.LTGRAY);
+                case R.id.Size1:
+                    if(stateChanged1){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
                     }else{
                         v.setBackgroundColor(Color.GREEN);
                     }
-                    stateChanged = !stateChanged;
+                    stateChanged1 = !stateChanged1;
+                    break;
+
+                case R.id.Size2:
+                if(stateChanged2){
+                    v.setBackgroundColor(Color.GREEN);
+                }else{
+                    v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                }
+                stateChanged2 = !stateChanged2;
+                break;
+
+                case R.id.Size3:
+                    if(stateChanged3){
+                        v.setBackgroundColor(Color.GREEN);
+                    }else{
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }
+                    stateChanged3 = !stateChanged3;
+                    break;
+
+                case R.id.SecondCol1:
+                    if(stateChanged4){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged4 = !stateChanged4;
+                    break;
+
+                case R.id.SecondCol2:
+                    if(stateChanged5){
+                        v.setBackgroundColor(Color.GREEN);
+                    }else{
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }
+                    stateChanged5 = !stateChanged5;
+                    break;
+
+                case R.id.SecondCol3:
+                    if(stateChanged6){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged6 = !stateChanged6;
+                    break;
+
+                case R.id.ThirdCol1:
+                    if(stateChanged7){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged7 = !stateChanged7;
+                    break;
+
+                case R.id.ThirdCol2:
+                    if(stateChanged8){
+                        v.setBackgroundColor(Color.GREEN);
+                    }else{
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }
+                    stateChanged8 = !stateChanged8;
+                    break;
+
+                case R.id.ThirdCol3:
+                    if(stateChanged9){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged9 = !stateChanged9;
+                    break;
+
+                case R.id.FourthCol1:
+                    if(stateChanged10){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged10 = !stateChanged10;
+                    break;
+
+                case R.id.FourthCol2:
+                    if(stateChanged11){
+                        v.setBackgroundColor(Color.GREEN);
+                    }else{
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }
+                    stateChanged11 = !stateChanged11;
+                    break;
+
+                case R.id.FourthCol3:
+                    if(stateChanged12){
+                        v.setBackgroundColor(Color.parseColor("#9aaff4"));
+                    }else{
+                        v.setBackgroundColor(Color.GREEN);
+                    }
+                    stateChanged12 = !stateChanged12;
                     break;
             }
         }
